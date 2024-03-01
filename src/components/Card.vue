4872b10e0c8bc988bb9cd946df1d0fe86a60ce03
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-02-29 22:48:13 -0300

    Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card">
+      <img :src="empleo.imagen" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">{{ empleo.cargo }}</h5>
+        <p class="card-text">{{ empleo.nombre_empresa }}</p>
+        <p class="card-text">{{ empleo.modalidad_trabajo }}</p>
+        <p class="card-text">{{ empleo.salario }}</p>
+        <p class="card-text">{{ empleo.vacantes }}</p>
+        <p class="card-text">{{ empleo.hora_publicacion }}</p>
+        <ul>
+          <li v-for="(habilidad, index) in empleo.habilidades_requeridas" :key="index">{{ habilidad }}</li>
+        </ul>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      empleo: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Estilos específicos del componente */
+  </style>
+  
