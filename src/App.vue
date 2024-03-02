902117de5a91624f888a4a5fbc44f79536e351cf
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-03-02 00:11:47 -0300

    components structured -side-search-nav && card in their positions && searchBar stiky

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,59 @@
 <template>
-  <div >
-    
-    <SearchBar/>
+  <div>
     <NavBar />
-    <SideBar/>
-   
-    <div class="row card-style">
-      <div class="col-md-12 ps-3" v-for="(empleo, index) in empleos" :key="index">
-        <Card :empleo="empleo" />
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-1">
+          <SideBar />
+        </div>
+        <div class="col-md-11">
+          <div class="mt-3 sticky-search-bar">
+            <SearchBar />
+          </div>
+          <div class="row card-style">
+            <div class="col-md-12 ps-3" v-for="(empleo, index) in empleos" :key="index">
+              <Card :empleo="empleo" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    
   </div>
 </template>
 
 <script>
-import Card from './components/Card.vue';  
-import SideBar from './components/SideBar.vue';
-import Empresas from './data/Empresas.json'; 
-import NavBar from './components/NavBar.vue'
-import SearchBar from './components/SearchBar.vue'
-
+import Card from "./components/Card.vue";
+import SideBar from "./components/SideBar.vue";
+import Empresas from "./data/Empresas.json";
+import NavBar from "./components/NavBar.vue";
+import SearchBar from "./components/SearchBar.vue";
 
 export default {
   components: {
     Card,
-    SideBar, 
+    SideBar,
     NavBar,
-    SearchBar
+    SearchBar,
   },
   data() {
     return {
-      empleos: Empresas.empleos 
+      empleos: Empresas.empleos,
     };
-  }
+  },
 };
 </script>
 
 <style scoped>
-.card-style{
+.card-style {
   background-color: rgb(236, 239, 241);
   opacity: 1;
+}
 
+.sticky-search-bar {
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  z-index: 1000; 
+  background-color: white; 
 }
 </style>
