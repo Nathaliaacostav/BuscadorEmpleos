07509e01aa337abb0953e9d70d86798a9dfac1f5
juanchos85 <86373520+juanchos85@users.noreply.github.com>
2024-03-02 09:42:38 -0300

    search

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -10,7 +10,7 @@ const count = ref(0);
 
 <template>
     <div>
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg bg-light ">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar scroll</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
